a548254d799e6a9e2a8af3f33d50b3a0f8e91dad
Shresth Prasad <shresthprasad7@gmail.com>
2025-04-26 02:50:13 +0530

    move draw menu function into it's own file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::collections::HashSet;
 use raylib::prelude::*;
 
+mod draw_call;
+use draw_call::draw_ring_menu;
+
 mod input_check;
 use input_check::{key_bind_pressed, mouse_wheel_scrolled};
 
 const WIN_W: i32 = 1920;
 const WIN_H: i32 = 1080;
-
-const COLOR_TRANSLUCENT_BLUE: Color = Color::new(100, 149, 237, 77);
-const COLOR_DARK_BLUE: Color = Color::new(31, 102, 229, 220);
 
 fn main() {
     let modifiers: HashSet<KeyboardKey> = vec![KeyboardKey::KEY_LEFT_CONTROL, KeyboardKey::KEY_LEFT_SHIFT].into_iter().collect();
@@ -53,56 +53,6 @@ fn main() {
         }
 
         draw_ring_menu(&mut d, screen_h, screen_w, segments, wheel_idx);
-    }
-}
-
-fn draw_ring_menu(
-    d: &mut RaylibDrawHandle,
-    screen_h: f32,
-    screen_w: f32,
-    segments: u32,
-    highlight: Option<u32>,
-) {
-
-    let center = Vector2::new(screen_w / 2.0, screen_h / 2.0);
-    let outer_radius = screen_h.min(screen_w) * 0.25;
-    let inner_radius = outer_radius * 0.75;
-
-    let gap_angle = 2.0;
-    let total_gap = gap_angle * segments as f32;
-    let angle_per_segment = (360.0 - total_gap) / segments as f32;
-
-    let mut start_angle = -90.0;
-    for idx in 0..segments {
-        let end_angle = start_angle + angle_per_segment;
-
-        let color = match highlight {
-            Some(h_idx) => {
-                assert!(
-                    h_idx <= segments,
-                    "hightlight index {} out of bounds for segments {}",
-                    h_idx, segments
-                );
-                if h_idx == idx {
-                    COLOR_DARK_BLUE
-                } else {
-                    COLOR_TRANSLUCENT_BLUE
-                }
-            },
-            None => COLOR_TRANSLUCENT_BLUE
-        };
-
-        d.draw_ring(
-            center,
-            inner_radius,
-            outer_radius,
-            start_angle,
-            end_angle,
-            0,
-            color
-        );
-
-        start_angle = end_angle + gap_angle;
     }
 }
 
