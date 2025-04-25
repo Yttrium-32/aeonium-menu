15f52338c4187d5dcbdc4b1feeeb44e645292f48
Shresth Prasad <shresthprasad7@gmail.com>
2025-04-26 02:50:13 +0530

    refactor: Move draw menu function into it's own file

---
+++ src/draw_call.rs
@@ -0,0 +1,55 @@
+use raylib::prelude::*;
+
+const COLOR_TRANSLUCENT_BLUE: Color = Color::new(100, 149, 237, 77);
+const COLOR_DARK_BLUE: Color = Color::new(31, 102, 229, 220);
+
+pub fn draw_ring_menu(
+    d: &mut RaylibDrawHandle,
+    screen_h: f32,
+    screen_w: f32,
+    segments: u32,
+    highlight: Option<u32>,
+) {
+
+    let center = Vector2::new(screen_w / 2.0, screen_h / 2.0);
+    let outer_radius = screen_h.min(screen_w) * 0.25;
+    let inner_radius = outer_radius * 0.75;
+
+    let gap_angle = 2.0;
+    let total_gap = gap_angle * segments as f32;
+    let angle_per_segment = (360.0 - total_gap) / segments as f32;
+
+    let mut start_angle = -90.0;
+    for idx in 0..segments {
+        let end_angle = start_angle + angle_per_segment;
+
+        let color = match highlight {
+            Some(h_idx) => {
+                assert!(
+                    h_idx <= segments,
+                    "hightlight index {} out of bounds for segments {}",
+                    h_idx, segments
+                );
+                if h_idx == idx {
+                    COLOR_DARK_BLUE
+                } else {
+                    COLOR_TRANSLUCENT_BLUE
+                }
+            },
+            None => COLOR_TRANSLUCENT_BLUE
+        };
+
+        d.draw_ring(
+            center,
+            inner_radius,
+            outer_radius,
+            start_angle,
+            end_angle,
+            0,
+            color
+        );
+
+        start_angle = end_angle + gap_angle;
+    }
+}
+
