2288f7c7cd681718ca973cb1228e39d654c35e51
Shresth Prasad <shresthprasad7@gmail.com>
2025-04-26 02:41:29 +0530

    allow mouse wheel to also control menu

--- src/input_check.rs
+++ src/input_check.rs
@@ -5,3 +5,12 @@ pub fn key_bind_pressed(modifier_keys: &HashSet<KeyboardKey>, main_key: Keyboard
     modifier_keys.iter().all(|&key| d.is_key_down(key)) && d.is_key_pressed(main_key)
 }
 
+pub fn mouse_wheel_scrolled(d: &RaylibDrawHandle) -> i32 {
+    let wheel_movement = d.get_mouse_wheel_move();
+    match wheel_movement {
+        w if w > 0.0 => 1,
+        w if w < 0.0 => -1,
+        _ => 0
+    }
+}
+
